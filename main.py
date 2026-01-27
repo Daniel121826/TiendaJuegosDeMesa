cde312158a1b012876924afa49db80157bebb3cc
Daniel121826 <alumno709866@ies-azarquiel.es>
2026-01-27 08:43:52 +0100

    Cors añadido

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database.database import Base, engine
 from routes import empleados, juegos_mesa
@@ -6,6 +7,16 @@ from routes import empleados, juegos_mesa
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+CORSMiddleware,
+allow_origins=[
+"http://localhost:5173", # React (Vite)
+"http://localhost:3000" # React (Create React App)
+],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
 
 app.include_router(empleados.router)
 app.include_router(juegos_mesa.router)
